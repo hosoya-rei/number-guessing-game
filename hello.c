f5b147340c525b7fafef2067288dddf700b0070c
Rei Hosoya <reirei.hoso.boz@gmail.com>
2022-10-29 23:52:41 +0000

    正解の数を0~9の乱数にした。

--- hello.c
+++ hello.c
@@ -1,9 +1,12 @@
 #include <stdio.h>
-
+#include <time.h>
+#include <stdlib.h>
 
 int main(void) {
-  int ans = 5;
+  int ans;
   int choice;
+  srand((unsigned int)time(NULL));
+  ans = rand() %9 +0;
   while (1) {
     printf("0~9の数字を入力:"); scanf("%d", &choice);
     if (ans == choice) {
